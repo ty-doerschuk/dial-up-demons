00ae83b99f77db2dad4545c54f046cac630dc9ea
Luis Ybarra & Joseph Scott <pair+lrybarra.scottjoseph@devbootcamp.com>
2016-03-30 14:46:03 -0500

    add new question link, form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,20 @@ get '/questions' do
   erb :'questions/index'
 end
 
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  @new_question = Question.new(params[:question])
+  if @new_question.save
+    redirect '/'
+  else
+    @errors = @new_question.errors.full_messages
+    erb :'questions/new'
+  end
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :'questions/show'

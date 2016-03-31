9a7fc9a1adf8c95ec2cfa2f30dda7e7c87ea4294
Ty Doerschuk <email": null>
2016-03-31 18:23:31 -0500

    add vote control

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,6 @@
 class Vote < ActiveRecord::Base
   validates_presence_of :voteable_id, :voteable_type
+  validates :voter, :uniqueness => { :scope => [:voteable_type, :voteable_id]}
 
   belongs_to :voter, class_name: "User"
   belongs_to :voteable, polymorphic: true

4617d9d397068cd4aff23479c83ae1aa908df28c
Luis Ybarra <lrybarra@gmail.com>
2016-03-31 17:36:47 -0500

    add favorite answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,9 +6,19 @@ class Question < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :votes, as: :voteable
   has_many :answers
-  has_one :favorite_answer, through: :answers
+  has_one :favorite_answer, class_name: "Answer"
 
   def vote_count
     self.votes
   end
-end
+
+#   def favorite_answer=(answer_id)
+#     self.favorite_answer = Answer.find(answer_id)
+#     #self.favorite_answer_id = @favorite_answer.id
+#     #self.save
+#   end
+
+#   def favorite_answer
+#     @favorite_answer
+#   end
+ end

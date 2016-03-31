1827b877245de5f40dce1f7d50464c504b3b8ef5
Ty Doerschuk <email": null>
2016-03-31 10:39:31 -0500

    Add basic voting functionality

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,3 +12,15 @@ post '/answers' do
   @answer = Answer.create(params[:answer])
   redirect "/questions/#{@answer.question_id}"
 end
+
+post '/answers/:id/votes' do
+  @answer = Answer.find(params[:id])
+  if params[:vote] == "vote up"
+    new_vote = @answer.votes.build(voter: current_user).save
+  elsif params[:vote] == "vote down"
+    if @answer.votes.count > 0
+      @answer.votes.first.destroy
+    end
+  end
+  redirect "/questions/#{@answer.question.id}"
+end
